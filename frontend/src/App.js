f273a0bb9655e107e546952b0e66abe944d78c72
Kal <info@kalacademy.org>
2022-11-06 11:04:35 -0800

    Created header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to my shop!</h1>
+    <Header />
+    <h1>Welcome to my shop!</h1>
+    <Footer />
     </>
   );
 }
