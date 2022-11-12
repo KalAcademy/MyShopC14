1ae13a9524df45fc6c9013d63750bf3792a2744a
Kal <info@kalacademy.org>
2022-11-12 11:03:20 -0800

    Product and homescreen components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import {Container} from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
     <Header />
-    <h1>Welcome to my shop!</h1>
+    <main>
+      <Container>
+        <HomeScreen />
+      </Container>
+    </main>
     <Footer />
     </>
   );
