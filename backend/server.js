bb136ebe9ed7b54fda05e8c3e641051da4df6976
Kal <info@kalacademy.org>
2022-12-11 11:07:55 -0800

    start of redux

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import productRoutes from './routes/productRoutes.js'
 import connectDB from './config/db.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 const app = express()
 dotenv.config()
 connectDB()
 
 app.use('/api/products', productRoutes)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
---
+++ frontend/src/store.js
@@ -0,0 +1,13 @@
+import {configureStore} from '@reduxjs/toolkit'
+
+
+const store = configureStore({
+  reducer: {},
+  preloadedState: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    immutableCheck: false,
+    serializableCheck: false,
+  })
+})
+
+export default store
