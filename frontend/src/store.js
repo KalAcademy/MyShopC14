285f04e3d0ba80eb667222e2402f8659864bd51e
Kal <info@kalacademy.org>
2023-01-07 11:02:15 -0800

    Cart

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,22 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const rootReducer = combineReducers({
   productList: productListReducer,
-  productDetails: productDetailsReducer
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
 })
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+  cart: {cartItems: cartItemsFromStorage}
+}
 
 const store = configureStore({
   reducer: rootReducer,
-  preloadedState: {},
+  preloadedState: initialState,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     immutableCheck: false,
     serializableCheck: false,
